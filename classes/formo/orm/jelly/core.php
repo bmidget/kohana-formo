2bd481601bd0e2523bea448314a6b9a6ce9e7adb
Ben Midget <benmidget@gmail.com>
2010-06-28 00:04:45 -0600

    Added support for BelongsTo and HasOne out of the box for Jelly integration

--- classes/formo/orm/jelly/core.php
+++ classes/formo/orm/jelly/core.php
@@ -56,13 +56,38 @@ class Formo_ORM_Jelly_Core extends Formo_ORM {
 			
 			// Determine the driver
 			$options['driver'] = $this->determine_driver($options, get_class($field));
-			
+						
 			// Add the value
-			$options['value'] = ($model->get($column))
-				? $model->get($column)
-				: $options['default'];
+			if ($field instanceof Jelly_Field_Relationship === FALSE)
+			{
+				// Add the value
+				$options['value'] = ($model->get($column))
+					? $model->get($column)
+					: $options['default'];
+			}
+			// Only perform this on BelongsTo and HasOne relationships
+			elseif ($field instanceof ManyToMany === FALSE AND $field instanceof HasMany === FALSE)
+			{
+				// grab the actual foreign model
+				$foreign_model = $model->get($column)->execute();
+				// Set the value
+				$options['value'] = $foreign_model->{$foreign_model->meta()->primary_key()};
+			}
+			else
+			{
+				// Grab the foreign records
+				$foreign_models = $model->get($column)->execute();
+				// Create the array
+				$values = array();
+				foreach ($foreign_model as $record)
+				{
+					$values[$record->get($record->name_key())] = $record->get($record->primary_key());
+				}
+				
+				$options['value'] = $values;
+			}
 			
-			// Convert value to at a string if it was an object	
+			// Convert value to at a string if it was an object
 			(is_object($options['value']) AND $options['value'] = (string) $options['value']);
 			
 			// Add the field to its parent
@@ -80,15 +105,34 @@ class Formo_ORM_Jelly_Core extends Formo_ORM {
 	public function pre_render()
 	{
 		if ((bool) $this->model === FALSE)
+			// Do nothing if the model is not set
 			return;
 			
 		foreach ($this->form->fields() as $field)
 		{
 			if ( ! $data = $this->model->meta()->fields($field->alias()))
+				// If field doesn't exist continue
 				continue;
 				
 			if ( ! $data instanceof Jelly_Field_Relationship)
-				continue;				
+				// If field is not a relationship, continue
+				continue;
+				
+			// Fetch the list of all available records
+			$records = Jelly::select($field->alias())
+				->order_by(':name_key')
+				->execute();
+			
+			// Create the array
+			$options = array();
+			foreach ($records as $record)
+			{
+				// Set the option
+				$options[$record->{$record->meta()->name_key()}] = $record->{$record->meta()->primary_key()};
+			}
+			
+			// Set all available options
+			$field->set('options', $options);
 		}
 	}
 	
@@ -119,7 +163,7 @@ class Formo_ORM_Jelly_Core extends Formo_ORM {
 			{
 				$check_value = $check_value($field);
 			}
-			
+						
 			// Check if the parameter indeed matches what it's supposed to
 			if ($field->get($parameter) === $check_value)
 			{
