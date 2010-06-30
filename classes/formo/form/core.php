425270a3217f9469ac3a34b653b4c2085903f4b9
Ben Midget <benmidget@gmail.com>
2010-06-29 22:00:45 -0600

    editable = FALSE fields won't let values be updated

--- classes/formo/form/core.php
+++ classes/formo/form/core.php
@@ -161,7 +161,11 @@ class Formo_Form_Core extends Formo_Validator {
 			return $this;
 		
 		foreach ($this->fields() as $field)
-		{				
+		{
+			if ($field->get('editable') === FALSE)
+				// Don't ever adjust values for not editable fields
+				continue;
+				
 			// post keys never have spaces
 			$input_key = str_replace(' ', '_', $field->alias());
 			
