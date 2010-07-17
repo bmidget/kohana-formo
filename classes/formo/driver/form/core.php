f662e113a5d6e403abab2dd02b1794ce063c5a4d
Ben Midget <benmidget@gmail.com>
2010-07-17 08:26:29 -0600

    Form allows action to be defined

--- classes/formo/driver/form/core.php
+++ classes/formo/driver/form/core.php
@@ -9,8 +9,10 @@ class Formo_Driver_Form_Core extends Formo_Driver {
 	public function html()
 	{
 		$this->render_field->set('tag', 'form')
-			->attr('method', $this->field->get('method', 'post'))
-			->attr('action', $this->field->get('action', ''));
+			->attr('method', $this->field->get('method', 'post'));
+		
+		// If it's not already defined, define the field's action	
+		(empty($this->render_field->attr['action']) AND $this->render_field->attr['action'] = '');
 	}
 
 }
