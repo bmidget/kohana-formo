2982e828050d59913414c66d18f52b23da8b0b68
Ben Midget <benmidget@gmail.com>
2010-07-29 08:10:34 -0600

    Fixed typo causing error

--- views/formo/html/checkboxes.php
+++ views/formo/html/checkboxes.php
@@ -1,7 +1,7 @@
 <?php echo $open; ?>
 	<label>
 		<?php echo $label; ?>
-		<?php echo $messagel ?>
+		<?php echo $message; ?>
 		<span class="field">
 			<?php foreach ($field->fields() as $checkbox): ?>
 				<?php echo $checkbox->render('html'); ?>
