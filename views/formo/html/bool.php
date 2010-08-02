b90ca3517127095a9e87e80f89519c0d0858a1f8
Ben Midget <benmidget@gmail.com>
2010-08-02 02:47:06 -0600

    Styling issue

--- views/formo/html/bool.php
+++ views/formo/html/bool.php
@@ -1,7 +1,7 @@
 <?php echo $open; ?>
 	<label>
 		<span class="input"><?php echo $field; ?></span>
-		<?php echo $label; ?>
+		<span class="label"><?php echo UTF8::ucfirst($field->label()); ?></span>
 	</label>
 	<?php echo $message; ?>
 <?php echo $close; ?>
