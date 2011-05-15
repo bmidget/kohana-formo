d3f005cdbf4baaeccc9a587c1e15d4205f74aae7
Ben Midget <benmidget@gmail.com>
2011-05-15 16:01:22 -0600

    Put the default form-level error inside the form tag

--- views/formo/html/form.php
+++ views/formo/html/form.php
@@ -1,8 +1,7 @@
-<?php if ($error = $form->error() AND $error !== TRUE): ?>
-	<span class="error-message"><?php echo UTF8::ucfirst($form->error()); ?></span>
-<?php endif; ?>
-
 <?php echo $form->open(); ?>
+	<?php if ($error = $form->error() AND $error !== TRUE): ?>
+		<span class="error-message"><?php echo UTF8::ucfirst($form->error()); ?></span>
+	<?php endif; ?>
 	<?php foreach ($form->fields() as $field): ?>
 		<?php echo $field->generate(); ?>
 	<?php endforeach; ?>
