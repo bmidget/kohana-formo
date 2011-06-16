bc7616a8721849febb769ec1d571f010669cfc37
Ben Midget <benmidget@gmail.com>
2011-06-16 00:50:53 -0600

    Couple group driver problems

--- views/formo/html/group.php
+++ views/formo/html/group.php
@@ -1,9 +1,9 @@
 <div class="group">
-	<h2><?php echo UTF8::ucfirst(str_replace('_', ' ', $field->alias())); ?></h2>
+	<h2><?php echo str_replace('_', ' ', $field->alias()); ?></h2>
 	<?php if ($error = $field->error() AND $error !== TRUE): ?>
 		<span class="error-message"><?php echo $field->error(); ?></span>
 	<?php endif; ?>
-	<?php foreach ($field->fields() as $field): ?>
-		<?php echo $view->render(); ?>
+	<?php foreach ($field->fields() as $_field): ?>
+		<?php echo $_field->render(); ?>
 	<?php endforeach; ?>
 </div>
