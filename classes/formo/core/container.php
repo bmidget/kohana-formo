a59e1ca53937a3f9565a5a0618b0447dd6e6557e
bmidget <benmidget@gmail.com>
2011-07-12 13:59:50 -0600

    Reversed last change

--- classes/formo/core/container.php
+++ classes/formo/core/container.php
@@ -174,12 +174,6 @@ abstract class Formo_Core_Container {
 		{
 			$value = $this->driver()->format_alias($value);
 		}
-		
-		if ($variable == 'val')
-		{
-			$this->val($value);
-			return $this;
-		}
 
 		if (array_key_exists($variable, $this->_defaults))
 		{
