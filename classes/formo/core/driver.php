d09ec87a6fbee66330781aa959fa84110ca98251
Ben Midget <benmidget@gmail.com>
2012-09-30 00:39:56 -0600

    Add driver method explanations

--- classes/formo/core/driver.php
+++ classes/formo/core/driver.php
@@ -2,16 +2,39 @@
 
 abstract class Formo_Core_Driver {
 
+	/**
+	 * Event that's run directly after a field is added
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return void
+	 */
 	public static function added( array $array)
 	{
 		return;
 	}
 
+	/**
+	 * Return whether a field can post nothing (ie, checkboxes post nothing if not checked)
+	 * 
+	 * @access public
+	 * @static
+	 * @return boolean
+	 */
 	public static function can_be_empty()
 	{
 		return false;
 	}
 
+	/**
+	 * Allow for any special closing tags that don't fit the norm (ie, datalist)
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return string
+	 */
 	public static function close( array $array)
 	{
 		$str = $array['str'];
@@ -19,11 +42,27 @@ abstract class Formo_Core_Driver {
 		return $str;
 	}
 
+	/**
+	 * Allow any pre-configured attributes for a field or form
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return array
+	 */
 	public static function get_attr( array $array)
 	{
 		return array();
 	}
 
+	/**
+	 * Return a formatted label
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return string
+	 */
 	public static function get_label( array $array)
 	{
 		$field = $array['field'];
@@ -35,26 +74,62 @@ abstract class Formo_Core_Driver {
 			: $field->alias();
 	}
 
+	/**
+	 * Configure and return any option fields (ie select, radios and checkboxes fields)
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return array
+	 */
 	public static function get_opts( array $array)
 	{
 		return array();
 	}
 
+	/**
+	 * Retrieve a template that renders options for a field
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return string
+	 */
 	public static function get_opts_template( array $array)
 	{
 		return NULL;
 	}
 
+	/**
+	 * Event run before validation. Allows pre-configured rules to be added
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return void
+	 */
 	public static function pre_validate( array $array)
 	{
 		return;
 	}
 
-	public static function get_tag()
-	{
-		
-	}
+	/**
+	 * Return a field's tag name. Has to be set for every field.
+	 * 
+	 * @access public
+	 * @static
+	 * @return string
+	 */
+	public static function get_tag() {}
 
+	/**
+	 * Return a field's template used with $field->render()
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return void
+	 */
 	public static function get_template( array $array)
 	{
 		$field = $array['field'];
@@ -67,16 +142,40 @@ abstract class Formo_Core_Driver {
 		return 'field_template';
 	}
 
+	/**
+	 * Get special title (used mostly for groups of fields like checkboxes and radios)
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return string
+	 */
 	public static function get_title( array $array)
 	{
-		return null;
+		return NULL;
 	}
 
+	/**
+	 * Return a field's value
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return mixed
+	 */
 	public static function get_val( array $array)
 	{
 		return $array['val'];
 	}
 
+	/**
+	 * Find values that will be added to the field's validation object
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return void
+	 */
 	public static function get_validation_values( array $array)
 	{
 		$field = $array['field'];
@@ -84,16 +183,26 @@ abstract class Formo_Core_Driver {
 		return array($field->alias() => $field->val());
 	}
 
+	/**
+	 * Return whether this is usually a parent containing multiple fields
+	 * 
+	 * @access public
+	 * @static
+	 * @return boolean
+	 */
 	public static function is_a_parent()
 	{
 		return FALSE;
 	}
 
-	public function is_changed()
-	{
-		
-	}
-
+	/**
+	 * Return field's open tag
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return string
+	 */
 	public static function open( array $array)
 	{
 		$str = $array['str'];
@@ -101,6 +210,14 @@ abstract class Formo_Core_Driver {
 		return $str;
 	}
 
+	/**
+	 * Driver actually sets a field's value from Formo::load()
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return void
+	 */
 	public static function load( array $array)
 	{
 		$val = $array['val'];
@@ -109,6 +226,14 @@ abstract class Formo_Core_Driver {
 		$field->val($val);
 	}
 
+	/**
+	 * Return a field's 'name' attribute
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return name
+	 */
 	public static function name( array $array)
 	{
 		$field = $array['field'];
@@ -131,6 +256,14 @@ abstract class Formo_Core_Driver {
 		return $name;
 	}
 
+	/**
+	 * Take a raw new value in and return what the field should interpret the value to be
+	 * 
+	 * @access public
+	 * @static
+	 * @param array $array
+	 * @return mixed
+	 */
 	public static function new_val( array $array)
 	{
 		$new_val = $array['new_val'];
