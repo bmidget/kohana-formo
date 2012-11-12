1a19f766a9f699a4aead7f2c64dfab0223ddca92
Ben Midget <benmidget@gmail.com>
2012-11-12 05:08:31 -0700

    support optgroups

--- views/formo_bootstrap/opts/select_template.php
+++ views/formo_bootstrap/opts/select_template.php
@@ -2,9 +2,21 @@
 <option></option>
 <?php endif; ?>
 <?php foreach ($opts as $key => $opt): ?>
-	<?php if ($field->val() == $key): ?>
-	<option value="<?=$key?>" selected="selected"><?=$opt?></option>
+	<?php if (is_array($opt)): ?>
+		<optgroup label="<?=$key?>">
+		<?php foreach ($opt as $_key => $_opt): ?>
+			<?php if ($field->val() == $_key): ?>
+			<option value="<?=$_key?>" selected="selected"><?=$_opt?></option>
+			<?php else: ?>
+			<option value="<?=$_key?>"><?=$_opt?></option>
+			<?php endif; ?>
+		<?php endforeach; ?>
+		</optgroup>
 	<?php else: ?>
-	<option value="<?=$key?>"><?=$opt?></option>
+		<?php if ($field->val() == $key): ?>
+		<option value="<?=$key?>" selected="selected"><?=$opt?></option>
+		<?php else: ?>
+		<option value="<?=$key?>"><?=$opt?></option>
+		<?php endif; ?>
 	<?php endif; ?>
 <?php endforeach; ?>
