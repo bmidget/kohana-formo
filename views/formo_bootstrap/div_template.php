fe39922ce0243e8f720d69817cb78a942d6cecdd
Felix Boerner <ich@felix-boerner.de>
2014-03-28 14:49:02 +0100

    Add a driver for div tags
    The driver inserts <div></div> tags like a field into a form.
    It it possible to use this to insert CAPTCHA's inside a form.

---
+++ views/formo_bootstrap/div_template.php
@@ -0,0 +1,15 @@
+<div class="field form-group formo-<?php echo $field->get('driver'); ?><?php if ($error = $field->error()) echo ' has-error'; ?>" id="field-container-<?php echo $field->alias(); ?>">
+	<?php if ($title): ?>
+		<span class="title"><?php echo $title; ?></span>
+	<?php elseif ($label = $field->label()): ?>
+		<label><?php echo $label; ?></label>
+	<?php endif; ?>
+
+	<?php echo $field->open().$field->html().$field->render_opts().$field->close(); ?>
+
+	<?php if ($msg = $field->error()): ?>
+		<span class="help-block"><?php echo $msg; ?></span>
+	<?php elseif ($msg = $field->get('message')): ?>
+		<span class="help-block"><?php echo $msg; ?></span>
+	<?php endif; ?>
+</div>
