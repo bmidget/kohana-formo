744818f6e758b684eebd816de314c8fea425b303
Felix Boerner <ich@felix-boerner.de>
2014-03-28 13:49:49 +0100

    Remove all short open tags because it is better to use only one variant

--- views/formo/field_template.php
+++ views/formo/field_template.php
@@ -1,31 +1,31 @@
-<div class="field <?=$field->get('driver')?><?php if ($error = $field->error()) echo ' error'; ?>" id="field-container-<?=$field->alias()?>">
+<div class="field <?php echo $field->get('driver'); ?><?php if ($error = $field->error()) echo ' error'; ?>" id="field-container-<?php echo $field->alias(); ?>">
 	<?php if ($label): ?>
 	<label>
-		<span class="label"><?=$field->label()?></span>
+		<span class="label"><?php echo $field->label(); ?></span>
 		<?php if ($field->get('editable')): ?>
 		<span class="field">
-			<?=$field->open().$field->render_opts().$field->close()?>
+			<?php echo $field->open().$field->render_opts().$field->close(); ?>
 		</span>
 		<?php else: ?>
-		<span class="field uneditable"><?=$field->val()?></span>
+		<span class="field uneditable"><?php echo $field->val(); ?></span>
 		<?php endif; ?>
 		<?php if ($error): ?>
-		<span class="error-message"><?=$error?></span>
+		<span class="error-message"><?php echo $error; ?></span>
 		<?php endif; ?>
 	</label>
 	<?php else: ?>
 		<?php if ($title): ?>
-		<span class="title"><?=$title?></span>
+		<span class="title"><?php echo $title; ?></span>
 		<?php endif; ?>
 		<?php if ($field->get('editable')): ?>
 		<span class="field">
-			<?=$field->open().$field->render_opts().$field->close()?>
+			<?php echo $field->open().$field->render_opts().$field->close(); ?>
 		</span>
 		<?php else: ?>
-		<span class="field uneditable"><?=$field->val()?></span>
+		<span class="field uneditable"><?php echo $field->val(); ?></span>
 		<?php endif; ?>
 		<?php if ($error): ?>
-		<span class="error-message"><?=$error?></span>
+		<span class="error-message"><?php echo $error; ?></span>
 		<?php endif; ?>
 	<?php endif; ?>
 </div>
